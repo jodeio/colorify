e390c70a6e38d6492525532d11e5da35a86ed429
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2018-03-28 01:36:04 +0800

    Add/Setup base function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      backgroundColor: ""
+    }
+  }
+
+  handleKeyPress = (e) => {
+    this.convertHex(e.target.value);
+  }
+
+  convertHex = (hex) =>{
+    hex = hex.replace(/\D/g,'');
+    var r = parseInt(hex.substring(0,2), 16);
+    var g = parseInt(hex.substring(2,4), 16);
+    var b = parseInt(hex.substring(4,6), 16);
+
+    var result = 'rgb('+r+','+g+','+b+')';
+  }
+  
+  componentDidMount(){
+    this.setState({
+      backgroundColor: "#F2F"
+    })
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Main className="wrap" style={{background: this.state.backgroundColor}}>
+        <Hex id="hex" placeholder="hex" autocomplete="off" onKeyUp={this.handleKeyPress}>
+        </Hex>
+        <RGB id="rgb" placeholder="rgb" autocomplete="off" onKeyUp={this.handleKeyPress}>
+        </RGB>
+      </Main>
     );
   }
 }
+
+const Main = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+`
+
+const Hex = styled.input`
+
+`
+
+const RGB = styled.input`
+
+`
 
 export default App;
