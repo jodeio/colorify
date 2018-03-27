3dc47a6b7d7144c7febbd3a80a53ca494473eb4e
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2018-03-28 02:15:51 +0800

    Add/Setup style rendering

--- src/App.js
+++ src/App.js
@@ -6,37 +6,60 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      backgroundColor: ""
+      theme: "light",
+      defaultBackgroundColor: "#F1C40F",
+      backgroundColor: "#F1C40F"
     }
   }
 
   handleKeyPress = (e) => {
-    this.convertHex(e.target.value);
+    // TODO: Add stubs here to support 8 digit hex
+    if(e.target.value.length === 3 || e.target.value.length === 6){
+      this.convertHex(e.target.value);
+    }else{
+      this.setState({
+        backgroundColor: this.state.defaultBackgroundColor,
+        theme: "light"
+      })
+    }
   }
 
   convertHex = (hex) =>{
     hex = hex.replace(/#/g,'');
-    var r = parseInt(hex.substring(0,2), 16);
-    var g = parseInt(hex.substring(2,4), 16);
-    var b = parseInt(hex.substring(4,6), 16);
+    
+    // TODO: Convert to a reusable method
+    var r = hex.length === 3 ? parseInt(hex.charAt(0) + hex.charAt(0), 16) : parseInt(hex.substring(0,2), 16);
+    var g = hex.length === 3 ? parseInt(hex.charAt(1) + hex.charAt(1), 16) : parseInt(hex.substring(2,4), 16);
+    var b = hex.length === 3 ? parseInt(hex.charAt(2) + hex.charAt(2), 16) : parseInt(hex.substring(4,6), 16);
 
+    // Assign rgb equivalent
     var result = 'rgb('+r+','+g+','+b+')';
  
     this.setState({
       backgroundColor: result
     })
+
+    // Check luminance
+    var lum = (r + g + b) / 3;
+
+    if(lum < 128){
+      this.setState({
+        theme: "dark"
+      })
+    }else{
+      this.setState({
+        theme: "light"
+      })
+    }
   }
   
   componentDidMount(){
     // TODO: Add stubs here for initial background randomization
-    this.setState({
-      backgroundColor: "#F2F"
-    })
   }
   
   render() {
     return (
-      <Main className="wrap" style={{background: this.state.backgroundColor}}>
+      <Main className={this.state.theme} style={{background: this.state.backgroundColor}}>
         <Hex id="hex" placeholder="hex" autocomplete="off" onKeyUp={this.handleKeyPress}>
         </Hex>
         <RGB id="rgb" placeholder="rgb" autocomplete="off" onKeyUp={this.handleKeyPress}>
